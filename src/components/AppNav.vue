a0e3359a9131e5f289f76f1413a65804b20634d0
Omkar <omkar.betha@gmail.com>
2021-07-22 22:44:44 +0530

    ss

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,7 +1,7 @@
 
 <template>
   <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
-    <a class="navbar-brand" href="#">70d0 0v3r3n61n33r1n6</a>
+    <a class="navbar-brand" href="#">Zelarsoft</a>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item" v-show="isAdmin()">
         <a class="btn btn-success mr-sm-2" href="#/admin">Admin</a>
